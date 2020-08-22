a754f236b0b5be8ed8c45de5e5309d1ef4f7713e
Sefer Demir <sefer.demir@primefor.com>
2020-08-22 22:56:52 +0300

    Operations were added.

---
+++ MarsRover/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRover
+{
+    class Program
+    {
+        static int bound_X;
+        static int bound_Y;
+        static int coor_X;
+        static int coor_Y;
+        static char or;
+
+        static void Main(string[] args)
+        {
+            string[] result = Test(new string[] { "5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM" });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        static string[] Test(string[] lines)
+        {
+            List<string> result = new List<string>();
+
+            int.TryParse(lines[0].Split(' ')[0], out bound_X);
+            int.TryParse(lines[0].Split(' ')[1], out bound_Y);
+
+            for (int i = 1; i < lines.Length; i += 2)
+            {
+                int.TryParse(lines[i].Split(' ')[0], out coor_X);
+                int.TryParse(lines[i].Split(' ')[1], out coor_Y);
+                char.TryParse(lines[i].Split(' ')[2], out or);
+
+                Rover rover = new Rover(coor_X, coor_Y, or);
+
+                for (int j = 0; j < lines[i + 1].Length; j++)
+                {
+                    rover.Control(lines[i + 1][j]);
+                }
+
+                result.Add(rover.GetPosition());
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,102 @@
+﻿namespace MarsRover
+{
+    public class Rover
+    {
+        private int coor_X { get; set; }
+        private int coor_Y { get; set; }
+        private char or { get; set; }
+
+        public Rover(int coor_X, int coor_Y, char or)
+        {
+            this.coor_X = coor_X;
+            this.coor_Y = coor_Y;
+            this.or = or;
+        }
+
+        public void SpinLeft()
+        {
+            switch (or)
+            {
+                case 'N':
+                    or = 'W';
+                    break;
+                case 'W':
+                    or = 'S';
+                    break;
+                case 'S':
+                    or = 'E';
+                    break;
+                case 'E':
+                    or = 'N';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void SpinRight()
+        {
+            switch (or)
+            {
+                case 'N':
+                    or = 'E';
+                    break;
+                case 'E':
+                    or = 'S';
+                    break;
+                case 'S':
+                    or = 'W';
+                    break;
+                case 'W':
+                    or = 'N';
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Move()
+        {
+            switch (or)
+            {
+                case 'N':
+                    coor_Y++;
+                    break;
+                case 'S':
+                    coor_Y--;
+                    break;
+                case 'E':
+                    coor_X++;
+                    break;
+                case 'W':
+                    coor_X--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Control(char command)
+        {
+            switch (command)
+            {
+                case 'L':
+                    SpinLeft();
+                    break;
+                case 'R':
+                    SpinRight();
+                    break;
+                case 'M':
+                    Move();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public string GetPosition()
+        {
+            return string.Format("{0} {1} {2}", coor_X, coor_Y, or);
+        }
+    }
+}
